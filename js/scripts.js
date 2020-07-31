2873bb2787a3b0896f9ac4abd5374d7b7668bace
Michael-Mbaya <gikundamike@gmail.com>
2020-07-31 22:11:11 +0300

    test size pricing and crust pricing

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,96 @@
+//object size (for pizza sizes)
 function size(name){
 	this.sizeName=name;
 	}
-	size.prototype.sizePrice=function(){
-		if(this.sizeName==="large"){
-		return 1000;
+//prototype for price of pizza sizes
+size.prototype.sizePrice=function(){
+	if(this.sizeName==="large"){
+		return 900;
 	}
+	else if(this.sizeName==="medium"){
+		return 650;
 	}
+	else if(this.sizeName==="small"){
+		return 450;
+	}
+	else{
+		alert("Please Input Pizza Size to continue");
+		$("form").reset();
+	}
+}
+
+//object crust (for pizza crusts)
+function crust(name){
+	this.crustName=name;
+}
+//prototype for price of pizza crusts
+crust.prototype.crustPrice=function(){
+	if(this.crustName==="crusty"){
+		return 100;
+	}
+	else if(this.crustName==="stuffed"){
+		return 200;
+	}
+	else if(this.crustName==="glutenFree"){
+		return 150;
+	}
+	else{
+		alert("Please Input Crust Type to continue");
+		$("form").reset();
+	}
+}
+
+//object topping (for pizza toppings)
+function topping(name){
+	this.toppingName=name;
+}
+//prototype for price of pizza toppings
+topping.prototype.toppingPrice=function(){
+	if(this.toppingName==="none"){
+		return 0;
+	}
+	else if((this.toppingName==="mushroom")&&($("#size").val()==="large")){
+		return 250;
+	}
+	else if((this.toppingName==="mushroom")&&($("#size").val()==="medium")){
+		return 160;
+	}
+	else if((this.toppingName==="mushroom")&&($("#size").val()==="small")){
+		return 100;
+	}
+	else if((this.toppingName==="sausage")&&($("#size").val()==="large")){
+		return 100;
+	}
+	else if((this.toppingName==="sausage")&&($("#size").val()==="medium")){
+		return 65;
+	}
+	else if((this.toppingName==="sausage")&&($("#size").val()==="small")){
+		return 35;
+	}
+	else if((this.toppingName==="onion")&&($("#size").val()==="large")){
+		return 50;
+	}
+	else if((this.toppingName==="onion")&&($("#size").val()==="medium")){
+		return 35;
+	}
+	else if((this.toppingName==="onion")&&($("#size").val()==="small")){
+		return 20;
+	}
+	else if(this.toppingName==="beef"){
+		return 150;
+	}
+	else if(this.toppingName==="chicken"){
+		return 200;
+	}
+	else{
+		alert("Please Input Crust Type to continue");
+		$("form").reset();
+	}
+}
+
+function pizzaTotal(){
+	
+}
 
 $(document).ready(function() {
 $("#submit").click(function(event){
@@ -17,11 +102,25 @@ $("#submit").click(function(event){
 	var inputAmountPizza=parseInt($("#quantity").val());
 
 	var sizePizza = new size(inputSize);
-	// alert(sizePizza.sizePrice());
 	alert(inputSize);
+	alert(sizePizza.sizePrice());									//test sizePrice
+	var crustPizza = new crust(inputCrust);
 	alert(inputCrust);
+	alert(crustPizza.crustPrice());								//test crustPrice
+	var toppingPizza = new topping(inputTopping);
 	alert(inputTopping);
-	alert(inputAmountPizza);
+	alert(toppingPizza.toppingPrice());						//test toppingPrice
+	// alert (parseInt(inputAmountPizza*(parseInt(sizePizza.sizePrice())+parseInt(crustPizza.crustPrice())+parseInt(toppingPizza.toppingPrice()))));
 
+
+	// function totalPizza(){
+	// 	event.preventDefault;
+	// 	if(inputAmountPizza===""){
+	// 		alert("Please Input Amount of Pizzas you Want!");
+	// 	}
+	// 	return (parseInt(inputAmountPizza*(parseInt(sizePizza.sizePrice())+parseInt(crustPizza.crustPrice())+parseInt(toppingPizza.toppingPrice()))));
+	// }
+	// alert(totalPizza());
+	// $("#formPizza").reset();
 });
 });
