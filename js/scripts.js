48038cd08953b3afdad660bb4b156893e425ba7a
Michael-Mbaya <gikundamike@gmail.com>
2020-08-02 20:16:13 +0300

    Checkout Prompt final

--- js/scripts.js
+++ js/scripts.js
@@ -141,35 +141,45 @@ $(document).ready(function() {
 													+". Total for This Order : "+inputAmountPizza+" Pizza *"+"("
 													+sizePizza.sizePrice()+"+"+crustPizza.crustPrice()+"+"
 													+toppingPizza.toppingPrice()+")"+" = "
-													+"<span class=\"totalPizza text-dark\">"+pizzaPrice+"</span>"+"</p>");
+                          +"<span class=\"totalPizza text-dark\">"+pizzaPrice+"</span>"+"</p>");
+    $("#addPizza").click();
+    $("#addPizza").focus();
 	}
 
 	$("#confirm").click(function(){
-		$("#submit").prop("disabled", true);
-		$("#addPizza").prop("disabled", true);
+		$("#submit").hide();
+    $("#addPizza").hide();
+    $("#confirm").hide();
 		$("#checkout").show();
-		$("#cancel").show();
+    $("#cancel").show();
 	})
 	$("#checkout").click(function(){
 		// var txt;
     var checkout=confirm("Would you like to Have it Delivered?\n Click OK for Delivery or CANCEL to Pick it up Yourself.");
     
 		if (checkout===false){
-      // txt = "You pressed Cancel!";
-		  // alert(txt);						//test cancel selection
 		  var sum = 0;
 		  $('.totalPizza').each(function(){
     		sum += parseFloat($(this).text());
       });
-      // alert(sum);           //test sum of all orders
-      // sum.stopImmediatePropagation();
-      $("#listOrders").append("<p class=\"grandTotal text-center\">"+"Your Total Bill : "+"<span class=\"totallBill text-danger font-weight-bold\">"+sum+"</span>"+"</p>");
       alert("Your Total Bill is:"+sum+"\n See Order Summary in Page Below.");
+      $("#listOrders").append("<p class=\"grandTotal text-center\">"+"Your Total Bill : "+"<span class=\"totallBill text-danger font-weight-bold\">"+sum+"</span>"+"</p>");
+      var contact=prompt("Enter Your Phone Number to receive a Order-receipt text.");
+      if (contact==="") {
+        alert("Please Enter a valid Number! \n Please Try Again");
+        $("#cancel").click();
+      } else {
+        alert("Dear Customer, an Order-Receipt has been sent to : "+contact+"\n Make sure to show the Text Upon Arrival while you wait for your Delicacy.");
+        $("#listOrders").append("<p>"+"Dear Customer, an Order-Receipt has been sent to : "+contact+" . Make sure to show the Text Upon Arrival while you wait for your Delicacy."+"</p>");
+        $("#listOrders").append("<p>"+"Thank You for Being our Customer. Mickey's Pizzeria Appreciates You!!!"+"</p>");
+        $(".totalBill").focus();
+        $("#checkout").hide();
+        $("#cancel").hide();
+        $("#formPizza").hide();
+      }
       checkout.stopImmediatePropagation();
     } 
     else{
-		  // txt = "You pressed OK!";
-      // alert(txt);						//test ok selection
       var locate=prompt("Enter Your Location");
         if (locate==="") {
           alert("Please Input Location for Delivery!");
@@ -180,14 +190,15 @@ $(document).ready(function() {
         });
         alert("Dear Customer, We will Deliver Your Order at : "+locate+".\n Please have Your Cash/Mpesa Equivalent of "+sum+"\n The Delivery person will give your Package upon Payment Only!");
         $("#listOrders").append("<p class=\"grandTotal text-center\">"+"Your Total Bill : "+"<span class=\"totallBill text-danger font-weight-bold\">"+sum+"</span>"+"</p>");
-      }
+        $("listOrders").append("<p>"+"Dear Customer, We will Deliver Your Order at : "+locate+". Please have Your Cash/Mpesa Equivalent of "+sum+". The Delivery person will give your Package upon Payment Only!"+"</p>");
+        $(".totalBill").focus();
+        $("#checkout").hide();
+        $("#cancel").hide();
+        $("#formPizza").hide();
+        }
       checkout.stopImmediatePropagation();
 		}
 	})
-
-	// var location = prompt('Please enter your Location');
-
-  // $("").append("<p>"+"You will pay " + newTotal + "</p>");
 
 });
 });
